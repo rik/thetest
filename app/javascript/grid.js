c9ac36a5e134d3b962b4b8abd5703df69c4e165b
Anthony Ricaud <anthony@ricaud.me>
2017-07-17 23:46:49 +0200

    Implement removeConnectedBlocksOfSameColourStartingAt
    Massive refactoring in only one commit. I've worked with smaller logical commits but in the context of a homework test, I'm not confident showing them. Sorry :(
    
    - Reimplement the grid as flexbox
      - CSS does not know about the number of columns or rows anymore
      - 0 on the y axis now represents the bottom of the grid, this is consistent with the majority of graphs
      - Removed dead CSS code
    - Use a function to generate colours rather than a class. The Block class had a notion of position that duplicates its position in the Grid. Getting rid of the position means we just need a colour generator.
    - Removed unnecessary grid size constants. This lets us reuse this module for different grid sizes, makes tests easier to write with smaller grids and does not come at the expense of a complex change.
    - Separate the grid logic from the rendering logic. This was done to simplify the tests.

--- app/javascript/grid.js
+++ app/javascript/grid.js
@@ -1,58 +1,119 @@
 export const COLOURS = ['red', 'green', 'blue', 'yellow'];
-const MAX_X = 10;
-const MAX_Y = 10;
 
-export class Block {
-  constructor(x, y) {
-    this.x = x;
-    this.y = y;
-    this.colour = COLOURS[Math.floor(Math.random() * COLOURS.length)];
-  }
+export function randomColour(x, y) {
+  return COLOURS[Math.floor(Math.random() * COLOURS.length)];
 }
 
-export class BlockGrid {
-  constructor() {
-    this.grid = [];
+export class ColouredGrid {
+  constructor(columnCount = 10, rowCount = 10, colourGenerator = randomColour) {
+    this._rowCount = rowCount;
+    this._columnCount = columnCount;
+    this._grid = [];
 
-    for (let x = 0; x < MAX_X; x++) {
+    for (let x = 0; x < columnCount; x++) {
       let col = [];
-      for (let y = 0; y < MAX_Y; y++) {
-        col.push(new Block(x, y));
+      for (let y = 0; y < rowCount; y++) {
+        col.push(colourGenerator(x, y));
       }
 
-      this.grid.push(col);
+      this._grid.push(col);
     }
-
-    return this;
   }
 
-  render(el = document.querySelector('#gridEl')) {
-    for (let x = 0; x < MAX_X; x++) {
-      let id = 'col_' + x;
-      let colEl = document.createElement('div');
-      colEl.className = 'col';
-      colEl.id = id;
-      el.appendChild(colEl);
-
-      for (let y = MAX_Y - 1; y >= 0; y--) {
-        let block = this.grid[x][y],
-          id = `block_${x}x${y}`,
-          blockEl = document.createElement('div');
-
-        blockEl.id = id;
-        blockEl.className = 'block';
-        blockEl.style.background = block.colour;
-        blockEl.addEventListener('click', evt => this.blockClicked(evt, block));
-        colEl.appendChild(blockEl);
-      }
-    }
-
-    return this;
+  columns() {
+    return this._grid;
   }
 
-  blockClicked(e, block) {
-    console.log(e, block);
+  removeConnectedBlocksOfSameColourStartingAt(x, y) {
+    const colourToRemove = this._grid[x][y];
+    const blocksToRemove = new Set();
+
+    this._selectBlocksToRemove(x, y, colourToRemove, blocksToRemove);
+    this._markBlocksToRemove(blocksToRemove);
+    this._removeMarkedBlocks();
+  }
+
+  _selectBlocksToRemove(x, y, colourToRemove, blocksToRemove) {
+    if (this._grid[x][y] != colourToRemove) {
+      return;
+    }
+    if (blocksToRemove.has(`${x},${y}`)) {
+      return;
+    }
+
+    blocksToRemove.add(`${x},${y}`);
+
+    if (x - 1 >= 0) {
+      this._selectBlocksToRemove(x - 1, y, colourToRemove, blocksToRemove);
+    }
+    if (x + 1 < this._columnCount) {
+      this._selectBlocksToRemove(x + 1, y, colourToRemove, blocksToRemove);
+    }
+    if (y - 1 >= 0) {
+      this._selectBlocksToRemove(x, y - 1, colourToRemove, blocksToRemove);
+    }
+    if (y + 1 < this._rowCount) {
+      this._selectBlocksToRemove(x, y + 1, colourToRemove, blocksToRemove);
+    }
+  }
+
+  _markBlocksToRemove(blocksToRemove) {
+    [...blocksToRemove.values()].forEach(coordinates => {
+      const [x, y] = coordinates.split(',').map(i => parseInt(i, 10));
+      delete this._grid[x][y];
+    });
+  }
+
+  _removeMarkedBlocks() {
+    for (const column in this._grid) {
+      this._grid[column] = this._grid[column].filter(Boolean);
+    }
   }
 }
 
-window.addEventListener('DOMContentLoaded', () => new BlockGrid().render());
+export class ColouredGridDOMRenderer {
+  constructor(grid, el = document.querySelector('#gridEl')) {
+    this._grid = grid;
+    this._el = el;
+    this._rowCount = grid.columns()[0].length;
+
+    this._render();
+  }
+
+  _render() {
+    this._el.innerHTML = '';
+
+    const columns = this._grid.columns();
+    columns.forEach((column, x) => {
+      const colEl = document.createElement('div');
+      colEl.classList.add('col');
+
+      column.forEach((colour, y) => {
+        const blockEl = document.createElement('div');
+        blockEl.classList.add('block');
+        blockEl.style.backgroundColor = colour;
+        blockEl.style.height = `calc(100% / ${this._rowCount}`;
+
+        blockEl.addEventListener('click', _ => this._blockClicked(x, y));
+        colEl.appendChild(blockEl);
+      });
+
+      this._el.appendChild(colEl);
+    });
+  }
+
+  _blockClicked(x, y) {
+    this._grid.removeConnectedBlocksOfSameColourStartingAt(x, y);
+
+    // Re-rendering the whole grid is fast enough for a 10x10 grid on an iPhone 6.
+    // And simple.
+    //
+    // If we see cases of poor performance, we can change the algorithm to delete
+    // blocks that have been removed.
+    this._render();
+  }
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+  new ColouredGridDOMRenderer(new ColouredGrid());
+});
--- app/javascript/grid.test.js
+++ app/javascript/grid.test.js
@@ -1,19 +1,178 @@
-import { Block, COLOURS } from './grid';
+import {
+  randomColour,
+  ColouredGrid,
+  ColouredGridDOMRenderer,
+  COLOURS
+} from './grid';
 import { assert } from 'chai';
 
-describe('Block', function() {
-  it('is created with correct coordinates and one of the valid colours', function() {
+describe('randomColour', function() {
+  it('returns a randomColour for every coordinate', function() {
     let testCoords = [[1, 2], [4, 9], [0, 0]];
 
-    testCoords.forEach(testCoord => {
-      let block = new Block(...testCoord);
-      assert.equal(block.x, testCoord[0], 'x is set correctly');
-      assert.equal(block.y, testCoord[1], 'y is set correctly');
-      assert.ok(COLOURS.indexOf(block.colour) > -1, 'colour is valid');
+    testCoords.forEach(([x, y]) => {
+      assert.include(COLOURS, randomColour(x, y));
     });
   });
 });
 
-describe('BlockGrid', function() {
-  it('should be implemented!', function() {});
+describe('ColouredGrid', function() {
+  it('creates a grid of colours', function() {
+    const grid = new ColouredGrid();
+    for (const column of grid.columns()) {
+      for (const colour of column) {
+        assert.include(COLOURS, colour);
+      }
+    }
+  });
+
+  it('creates a red grid', function() {
+    function red(x, y) {
+      return COLOURS[0];
+    }
+
+    const grid = new ColouredGrid(10, 10, red);
+    for (const column of grid.columns()) {
+      for (const colour of column) {
+        assert.equal(colour, COLOURS[0]);
+      }
+    }
+  });
+
+  describe('#removeConnectedBlocksOfSameColourStartingAt', function() {
+    const testcases = [
+      {
+        before: [
+          // prettier-ignore
+          ['green', 'green', 'green'],
+          ['red', 'green', 'green'],
+          ['green', 'green', 'green']
+        ],
+        after: [
+          // prettier-ignore
+          ['green', 'green', 'green'],
+          ['green', 'green'],
+          ['green', 'green', 'green']
+        ],
+        startAt: { x: 1, y: 0 }
+      },
+      {
+        before: [
+          // prettier-ignore
+          ['green', 'green', 'green'],
+          ['red', 'red', 'red'],
+          ['green', 'green', 'green']
+        ],
+        after: [
+          // prettier-ignore
+          ['green', 'green', 'green'],
+          [],
+          ['green', 'green', 'green']
+        ],
+        startAt: { x: 1, y: 1 }
+      },
+      {
+        before: [
+          // prettier-ignore
+          ['green', 'red', 'green', 'green'],
+          ['red', 'red', 'red', 'red'],
+          ['green', 'red', 'green', 'green']
+        ],
+        after: [
+          // prettier-ignore
+          ['green', 'green','green'],
+          [],
+          ['green', 'green', 'green']
+        ],
+        startAt: { x: 1, y: 1 }
+      },
+      {
+        before: [
+          // prettier-ignore
+          ['red', 'red', 'green', 'red'],
+          ['red', 'red', 'green', 'green'],
+          ['green', 'red', 'red', 'red']
+        ],
+        after: [
+          // prettier-ignore
+          ['green', 'red'],
+          ['green', 'green'],
+          ['green']
+        ],
+        startAt: { x: 1, y: 1 }
+      }
+    ];
+
+    function makeColourGenerator(before) {
+      return function(x, y) {
+        return before[x][y];
+      };
+    }
+
+    testcases.forEach(({
+      before,
+      after,
+      startAt
+    }, index) => {
+      it(`case ${index}`, function() {
+        const grid = new ColouredGrid(
+          before.length,
+          before[0].length,
+          makeColourGenerator(before)
+        );
+
+        const blocksToRemove = grid.removeConnectedBlocksOfSameColourStartingAt(
+          startAt.x,
+          startAt.y
+        );
+        assert.deepEqual(grid.columns(), after);
+      });
+    });
+  });
+});
+
+describe('ColouredGridDOMRenderer', function() {
+  let el;
+  let grid;
+  const colouredGrid = [
+    ['red', 'green', 'blue'],
+    ['green', 'blue', 'red'],
+    ['blue', 'red', 'green']
+  ];
+
+  before(function() {
+    grid = {
+      columns() {
+        return colouredGrid;
+      }
+    };
+    el = document.createElement('div');
+    new ColouredGridDOMRenderer(grid, el);
+  });
+
+  it('assigns background colours', function() {
+    const backgroundColors = [...el.childNodes].map(column => {
+      return [...column.childNodes].map(block => block.style.backgroundColor);
+    });
+
+    assert.deepEqual(colouredGrid, backgroundColors);
+  });
+
+  it('calls removeConnectedBlocksOfSameColourStartingAt when clicking blocks', function() {
+    let xCalled;
+    let yCalled;
+    grid.removeConnectedBlocksOfSameColourStartingAt = function(x, y) {
+      xCalled = x;
+      yCalled = y;
+    };
+
+    el.firstChild.firstChild.click();
+
+    assert.strictEqual(xCalled, 0);
+    assert.strictEqual(yCalled, 0);
+
+    el.lastChild.lastChild.click();
+    assert.strictEqual(xCalled, 2);
+    assert.strictEqual(yCalled, 2);
+  });
 });
